c8eb7d22754760cd876d79bd40cce8bf968e42a9
Jiri Hausner <jiri.hausner@scalateams.com>
2026-02-23 19:11:19 +0100

    feat: upgrade mill to 1.1.2, extract shared generator definitions to GeneratorModule, add ParserRef and make parsers and configuration cached task

--- mill-zio-http-gen/src/org/scalateams/mill/zio/http/gen/api.scala
+++ mill-zio-http-gen/src/org/scalateams/mill/zio/http/gen/api.scala
@@ -2,10 +2,4 @@ package org.scalateams.mill.zio.http.gen.api
 
 import mill.api.PathRef
 
-case class GenerationResult(dest: PathRef, files: Seq[PathRef])
-
-object GenerationResult {
-
-  implicit val jsonFormatter: upickle.default.ReadWriter[GenerationResult] =
-    upickle.default.macroRW
-}
+final case class GenerationResult(dest: PathRef, files: Seq[PathRef]) derives upickle.default.ReadWriter
